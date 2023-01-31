13c89dc0d4f8d6e1c9b65f1e1241a0fccbb94de8
oficialrivas <oficialrivasbrito14@gmail.com>
2023-01-30 21:57:08 -0400

    cambio

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -213,7 +213,7 @@ export default function Example({a}) {
                   type="button"
                   className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  <span className="sr-only">View notifications</span>
+                  <span className="sr-only">View </span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
